7809daff9453db93dc6cbe7386e4550c85a3b6c2
Justin Browne <brownej@nscl.msu.edu>
2016-09-12 16:16:13 -0400

    Implement new is_inside algorithm for Cut2dPoly

--- src/cut.rs
+++ src/cut.rs
@@ -46,102 +46,48 @@ impl Cut2dPoly {
             verts: verts,
         }
     }
-
-    fn edges(&self) -> Vec<Edge> {
-        let mut edges = Vec::<Edge>::new();
-
-        for i in 0..(self.verts.len()) {
-            let v1 = self.verts[i];
-            let v2 = if i == (self.verts.len() - 1) {
-                self.verts[0]
-            } else {
-                self.verts[i+1]
-            };
-
-            edges.push(
-                Edge{
-                    x1: v1.0,
-                    y1: v1.1,
-                    x2: v2.0,
-                    y2: v2.1,
-                }
-                );
-        }
-
-        edges
-    }
 }
 
 impl Cut2d for Cut2dPoly {
     fn is_inside(&self, x: f64, y: f64) -> bool {
         let mut inside = false;
-        let edges = self.edges();
 
-        for (i, e) in edges.iter().enumerate() {
-            if (y > e.y1 && y < e.y2) || (y < e.y1 && y > e.y2) {
-                // Does the path pass through an edge?
-                if e.x_at(y) == x {
-                    return true;
-                }
-                if e.x_at(y) < x {
+        let mut j = self.verts.len() - 1;
+
+        for i in 0..self.verts.len() {
+            let x1 = self.verts[j].0;
+            let x2 = self.verts[i].0;
+            let y1 = self.verts[j].1;
+            let y2 = self.verts[i].1;
+
+            if ((y2 < y) && (y1 >= y)) || ((y1 < y) && (y2 >= y)) {
+                if (x2 + (y - y2) * (y1 - y2) / (x1 - x2)) < x {
                     inside = !inside;
                 }
-            } else if y == e.y2 {
-                // Does the path pass through a vertex?
-                if e.x2 == x {
-                    return true;
-                }
-                if e.x2 < x {
-                    let e2 = if i == (edges.len() - 1) {
-                        edges[0].clone()
-                    } else {
-                        edges[i + 1].clone()
-                    };
-                    if (e.y2 - e.y1).signum() == (e2.y2 - e2.y1).signum() {
-                        inside = !inside;
-                    }
-                }
             }
+
+            j = i;
         }
 
         inside
     }
 }
 
-#[derive(Debug, Clone)]
-struct Edge {
-    pub x1: f64,
-    pub y1: f64,
-    pub x2: f64,
-    pub y2: f64,
-}
-
-impl Edge {
-    fn y_at(&self, x: f64) -> f64 {
-        self.y1 + (x - self.x1) * (self.y2 - self.y1) / (self.x2 - self.x1)
-    }
-
-    fn x_at(&self, y: f64) -> f64 {
-        self.x1 + (y - self.y1) * (self.x2 - self.x1) / (self.y2 - self.y1)
-    }
-
-    fn slope(&self) -> f64 {
-        (self.y2 - self.y1) / (self.x2 - self.x1)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    const EP: f64 = 3. * ::std::f64::EPSILON;
 
     #[test]
     fn circ_is_inside() {
         let c = Cut2dCirc::new(0f64, 0f64, 0f64);
-        assert!(!c.is_inside(1f64, 0f64));
-        assert!(!c.is_inside(0f64, 1f64));
-        assert!(c.is_inside(0f64, 0f64));
+        assert!(!c.is_inside(0f64, 0f64 + EP));
+        assert!(!c.is_inside(0f64 + EP, 0f64));
+        assert!(!c.is_inside(0f64, 0f64 - EP));
+        assert!(!c.is_inside(0f64 - EP, 0f64));
 
         let c = Cut2dCirc::new(1f64, 0f64, -1f64);
+        //FIXME
         assert!(!c.is_inside(-1f64, 0f64));
         assert!(c.is_inside(0f64, 0f64));
         assert!(c.is_inside(1f64, 0f64));
@@ -150,15 +96,94 @@ mod tests {
 
     #[test]
     fn poly_is_inside() {
-        let c = Cut2dPoly::from_verts(vec![(0f64, 0f64), (1f64, 1f64), (1f64, -1f64)]);
-        let c = Cut2dPoly::from_verts(vec![(1f64, -1f64), (1f64, 1f64), (0f64, 0f64)]);
-        assert!(!c.is_inside(-1f64, 0f64));
+        let c = Cut2dPoly::from_verts(vec![(0f64, -1f64), (2f64, -1f64), (4f64, 1f64),
+                                           (3f64, 1f64), (2f64, 1f64), (1f64, 0f64),
+                                           (0f64, 1f64), (-1f64, 1f64), (-2f64, 1f64)]);
+
+        assert!(!c.is_inside(-3f64, -2f64));
+        assert!(!c.is_inside(-3f64, -1f64));
+        assert!(!c.is_inside(-3f64, 0f64));
+        assert!(!c.is_inside(-3f64, 1f64));
+        assert!(!c.is_inside(-3f64, 2f64));
+
+        assert!(!c.is_inside(-2f64, -2f64));
+        assert!(!c.is_inside(-2f64, -1f64));
+        assert!(!c.is_inside(-2f64, 0f64));
+        assert!(!c.is_inside(-2f64, 1f64 + EP));
+        assert!(!c.is_inside(-2f64 - EP, 1f64));
+        assert!(!c.is_inside(-2f64, 1f64 - EP));
+        assert!(c.is_inside(-2f64 + 2. * EP, 1f64 - EP));
+        assert!(!c.is_inside(-2f64, 2f64));
+
+        assert!(!c.is_inside(-1f64, -2f64));
+        assert!(!c.is_inside(-1f64, -1f64));
+        assert!(c.is_inside(-1f64, 0f64 + EP));
+        assert!(c.is_inside(-1f64 + EP, 0f64));
+        assert!(!c.is_inside(-1f64, 0f64 - EP));
+        assert!(!c.is_inside(-1f64 - EP, 0f64));
+        assert!(!c.is_inside(-1f64 + EP, 1f64 + EP));
+        assert!(c.is_inside(-1f64 + EP, 1f64 - EP));
+        assert!(c.is_inside(-1f64 - EP, 1f64 - EP));
+        assert!(!c.is_inside(-1f64 - EP, 1f64 + EP));
+        assert!(!c.is_inside(-1f64, 2f64));
+
+        assert!(!c.is_inside(0f64, -2f64));
+        assert!(c.is_inside(0f64, -1f64 + EP));
+        assert!(!c.is_inside(0f64, -1f64 - EP));
+        assert!(!c.is_inside(0f64 - EP, -1f64));
+        assert!(c.is_inside(0f64, 0f64));
+        assert!(!c.is_inside(0f64, 1f64 + EP));
+        assert!(!c.is_inside(0f64 + EP, 1f64));
+        assert!(c.is_inside(0f64, 1f64 - EP));
         assert!(!c.is_inside(0f64, 2f64));
-        assert!(c.is_inside(0f64, 0f64));
-        assert!(c.is_inside(0f64, 0f64));
-        assert!(c.is_inside(1f64, 0f64));
-        assert!(c.is_inside(0.75f64, 0.25f64));
-        assert!(c.is_inside(0.75f64, -0.25f64));
-        assert!(c.is_inside(0.75f64, 0f64));
+
+        assert!(!c.is_inside(1f64, -2f64));
+        assert!(c.is_inside(1f64 + EP, -1f64 + EP));
+        assert!(!c.is_inside(1f64 + EP, -1f64 - EP));
+        assert!(!c.is_inside(1f64 - EP, -1f64 - EP));
+        assert!(c.is_inside(1f64 - EP, -1f64 + EP));
+        assert!(!c.is_inside(1f64, 0f64 + EP));
+        assert!(c.is_inside(1f64 + EP, 0f64));
+        assert!(c.is_inside(1f64, 0f64 - EP));
+        assert!(c.is_inside(1f64 - EP, 0f64));
+        assert!(!c.is_inside(1f64, 1f64));
+        assert!(!c.is_inside(1f64, 2f64));
+
+        assert!(!c.is_inside(2f64, -2f64));
+        assert!(c.is_inside(2f64, -1f64 + EP));
+        assert!(!c.is_inside(2f64 + EP, -1f64));
+        assert!(!c.is_inside(2f64, -1f64 - EP));
+        assert!(c.is_inside(2f64, 0f64));
+        assert!(!c.is_inside(2f64, 1f64 + EP));
+        assert!(c.is_inside(2f64, 1f64 - EP));
+        assert!(!c.is_inside(2f64 - EP, 1f64));
+        assert!(!c.is_inside(2f64, 2f64));
+
+        assert!(!c.is_inside(3f64, -2f64));
+        assert!(!c.is_inside(3f64, -1f64));
+        assert!(c.is_inside(3f64, 0f64 + EP));
+        assert!(!c.is_inside(3f64 + EP, 0f64));
+        assert!(!c.is_inside(3f64, 0f64 - EP));
+        assert!(c.is_inside(3f64 - EP, 0f64));
+        assert!(!c.is_inside(3f64 + EP, 1f64 + EP));
+        assert!(c.is_inside(3f64 + EP, 1f64 - EP));
+        assert!(c.is_inside(3f64 - EP, 1f64 - EP));
+        assert!(!c.is_inside(3f64 - EP, 1f64 + EP));
+        assert!(!c.is_inside(3f64, 2f64));
+
+        assert!(!c.is_inside(4f64, -2f64));
+        assert!(!c.is_inside(4f64, -1f64));
+        assert!(!c.is_inside(4f64, 0f64));
+        assert!(!c.is_inside(4f64, 1f64 + EP));
+        assert!(!c.is_inside(4f64 + EP, 1f64));
+        assert!(!c.is_inside(4f64, 1f64 - EP));
+        assert!(c.is_inside(4f64 - EP, 1f64 - EP));
+        assert!(!c.is_inside(4f64, 2f64));
+
+        assert!(!c.is_inside(5f64, -2f64));
+        assert!(!c.is_inside(5f64, -1f64));
+        assert!(!c.is_inside(5f64, 0f64));
+        assert!(!c.is_inside(5f64, 1f64));
+        assert!(!c.is_inside(5f64, 2f64));
     }
 }
