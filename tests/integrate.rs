cb59193148a82261bb7338abd836b374e01ec4fc
Justin Browne <brownej@nscl.msu.edu>
2017-02-23 15:36:10 -0500

    Add another integrate test

--- tests/integrate.rs
+++ tests/integrate.rs
@@ -1,7 +1,7 @@
 extern crate datakiste;
 extern crate rand;
 
-use datakiste::cut::{Cut1dLin, Cut2dCirc};
+use datakiste::cut::{Cut1dLin, Cut2dCirc, Cut2dPoly};
 use datakiste::hist::{Hist1d, Hist2d};
 use rand::distributions::{IndependentSample, Range};
 
@@ -63,4 +63,18 @@ fn integrate_2d_rand() {
 
     assert_eq!(h.integrate(&c1), 150);
     assert_eq!(h.integrate(&c2), 550);
+}
+
+#[test]
+fn integrate_2d_banana() {
+    let mut h = Hist2d::new(1001usize, 0f64, 1f64, 1001usize, 0f64, 1f64).unwrap();
+
+    h.fill_with_counts(0.25, 0.65, 100);
+    h.fill_with_counts(0.7, 0.51, 242);
+    h.fill_with_counts(8., 10., 342);
+    h.fill_with_counts(0., 0., 114);
+
+    let c1 = Cut2dPoly::from_verts(vec![(0.1, 0.75), (0.5, 0.65), (0.75, 0.5), (0.4, 0.5), (0.1, 0.7)]);
+
+    assert_eq!(h.integrate(&c1), 342);
 }
